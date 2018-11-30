5709e9074212802429915feea3c8012f530233ff
luke parker <lukeparker@lukes-MacBook-Pro.local>
2018-11-30 15:13:25 -0700

    Set up stack view and IBOutlets

--- buttonCounter/ViewController.swift
+++ buttonCounter/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    
 }
 
