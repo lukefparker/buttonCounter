c18e044baa32d28f823a1cf8980c9ceafb2e6b25
luke parker <lukeparker@lukes-macbook-pro.local>
2018-11-30 14:50:45 -0700

    Initial Commit

---
+++ buttonCounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  buttonCounter
+//
+//  Created by luke parker on 11/30/18.
+//  Copyright © 2018 luke parker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
